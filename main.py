e12406339d8493b7daab333d7fc2bebba21f6b71
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-12-25 15:03:28 +0300

    Pass

---
+++ main.py
@@ -0,0 +1,15 @@
+i=int(input("Type a number:"))
+if i % 20==0:
+    print("It is divisible by 20")
+
+elif i % 15==0:
+    pass
+
+elif i % 5==0:
+    print("It is divisible by 5")
+
+elif i % 3==0:
+    print("It is divisible by 3")
+
+else:
+    print(i)
